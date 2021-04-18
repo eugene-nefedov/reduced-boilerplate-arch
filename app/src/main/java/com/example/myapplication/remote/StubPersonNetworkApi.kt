2f1bc3a92e02e14c60f85e42754b101d1633a34e
Eugene Nefedov <eugene.fisher1993@gmail.com>
2021-04-18 20:25:48 +0200

    Networking simulation on IO dispatcher

--- app/src/main/java/com/example/myapplication/remote/StubPersonNetworkApi.kt
+++ app/src/main/java/com/example/myapplication/remote/StubPersonNetworkApi.kt
@@ -1,12 +1,15 @@
 package com.example.myapplication.remote
 
 import com.example.myapplication.data.PersonNetworkApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 class StubPersonNetworkApi : PersonNetworkApi {
 
-    override suspend fun getPerson(id: String): PersonNetworkResponse {
-        Thread.sleep(2000L)
-        return PersonNetworkResponse(
+    override suspend fun getPerson(id: String): PersonNetworkResponse = withContext(Dispatchers.IO) {
+        delay(2000L)
+        return@withContext PersonNetworkResponse(
             id = "42",
             firstName = "Sherlock",
             lastName = "Holmes",
